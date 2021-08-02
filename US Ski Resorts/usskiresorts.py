705560bb38d60652d2878225a4fc7e4fa2699919
Taylor Smith <tjsmith1513@gmail.com>
2021-08-02 16:08:56 -0400

    Added more info to pop ups changed html style

--- US Ski Resorts/usskiresorts.py
+++ US Ski Resorts/usskiresorts.py
@@ -11,16 +11,20 @@ m = folium.Map(location=[latitude, longitude], zoom_start=4)
 for i in range(0,len(data)):
     html ="""
     <div>
-      <h4>{resort} </h4>
-        <ul>
-          <li>Summit: {summit}</li>
-          <li>Base: {base}</li>
-          <li>Vertical: {vert}</li>
-          <li>Number of runs: {runs}</li>
-          <li>Number of lifts: {lifts}</li>
-        </ul> 
+      <p><strong>{resort}</strong></p>
+      <p style="margin:0;">Summit: {summit}</p>
+      <p style="margin:0;">Base: {base}</p>
+      <p style="margin:0;">Vertical: {vert}</p>
+      <p style="margin:0;">Number of runs: {runs}</p>
+      <p style="margin:0;">Number of lifts: {lifts}</p>
+      <p style="margin:0;">Green Runs: {green}%</p>
+      <p style="margin:0;">Blue Runs: {blue}%</p>
+      <p style="margin:0;">Black Runs: {black}%</p>
+
     </div>
-    """.format(base=data.iloc[i]['base'], summit= data.iloc[i]['summit'], resort=data.iloc[i]['resort_name'], vert=data.iloc[i]['vertical'], runs=data.iloc[i]['runs'], lifts=data.iloc[i]['lifts'])
+    """.format(base=data.iloc[i]['base'], summit= data.iloc[i]['summit'], resort=data.iloc[i]['resort_name'], vert=data.iloc[i]['vertical'],
+               runs=data.iloc[i]['runs'], lifts=data.iloc[i]['lifts'], green=data.iloc[i]['green_percent'], blue=data.iloc[i]['blue_percent'],
+               black=data.iloc[i]['black_percent'])
 
     iframe = folium.IFrame(html=html, width=200, height=100)
 
