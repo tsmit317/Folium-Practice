955b74903b412fd26886b326d94609e63f819b8c
Taylor Smith <tjsmith1513@gmail.com>
2021-08-02 15:33:14 -0400

    Adding more info to the popups. Working with html again

--- US Ski Resorts/usskiresorts.py
+++ US Ski Resorts/usskiresorts.py
@@ -9,10 +9,23 @@ m = folium.Map(location=[latitude, longitude], zoom_start=4)
 
 
 for i in range(0,len(data)):
-    iframe = folium.IFrame(data.iloc[i]['resort_name'], width=100, height=100)
+    html ="""
+    <div>
+      <h4>{resort} </h4>
+        <ul>
+          <li>Summit: {summit}</li>
+          <li>Base: {base}</li>
+          <li>Vertical: {vert}</li>
+          <li>Number of runs: {runs}</li>
+          <li>Number of lifts: {lifts}</li>
+        </ul> 
+    </div>
+    """.format(base=data.iloc[i]['base'], summit= data.iloc[i]['summit'], resort=data.iloc[i]['resort_name'], vert=data.iloc[i]['vertical'], runs=data.iloc[i]['runs'], lifts=data.iloc[i]['lifts'])
+
+    iframe = folium.IFrame(html=html, width=200, height=100)
 
     popup = folium.Popup(iframe,
-                     max_width=100)
+                     max_width=200)
     ski_icon = folium.features.CustomIcon('ski-icon2.png', icon_size=(25,25))
     folium.Marker(
       location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
