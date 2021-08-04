2b84e96cd7633b4c42427b2afe5327bbdbffefb0
Taylor Smith <tjsmith1513@gmail.com>
2021-08-04 10:24:46 -0400

    added popup data added tooltip

--- US Airports/airports.py
+++ US Airports/airports.py
@@ -11,27 +11,80 @@ md = folium.FeatureGroup(name="Medium Airports")
 
 
 for i in range(1,len(data)):
+    
+    
+        
+    
     if data.iloc[i]['type'] == 'large_airport':
-        iframe = folium.IFrame(data.iloc[i]['name'], width=100, height=100)
+        html ="""
+            
+            <div>
+            <p><strong>{name}</strong></p>
+            
+            <p style="margin:0;">Airport Code: {ident}</p>
+            <p style="margin:0;">Elevation: {elevation}ft</p>
+            <p style="margin:0;">Location: {city}, {state}</p>
+            
+            <a href="{wiki}" target="_blank">Wikipedia</a>
+            
+            
 
-        popup = folium.Popup(iframe,
-                        max_width=100)
-        lg_icon = folium.features.CustomIcon('lg_air_icon.png', icon_size=(25,25))
+            </div>
+            """.format( name=data.iloc[i]['name'], 
+                    ident= data.iloc[i]['ident'], 
+                    elevation= data.iloc[i]['elevation_ft'], 
+                    city=data.iloc[i]['municipality'], 
+                    state= data.iloc[i]['iso_region'][-2],
+                    wiki=data.iloc[i]['wikipedia_link'])
+        iframe = folium.IFrame(html=html, width=200, height=200)
+        tool_tip_html = """<div style="white-space: normal">{name} </div>""".format(name=data.iloc[i]['name'])
+        popup = folium.Popup(iframe, max_width=200)
+        lg_icon = folium.features.CustomIcon('img/lg_air_icon.png', icon_size=(25,25))
         lg.add_child(folium.Marker(
             location=[float(data.iloc[i]['latitude_deg']), float(data.iloc[i]['longitude_deg'])],
-            popup=popup, icon=lg_icon
+            popup=popup, 
+            icon=lg_icon,
+            tooltip=folium.Tooltip(
+                text=folium.Html(tool_tip_html, script=True, width=150).render(),
+            )
+
         ))
         
 
     elif data.iloc[i]['type'] == 'medium_airport':
-        iframe = folium.IFrame(data.iloc[i]['name'], width=100, height=100)
 
-        popup = folium.Popup(iframe,
-                        max_width=100)
-        md_icon = folium.features.CustomIcon('md_air_icon.png', icon_size=(15,15))
-        lg.add_child(folium.Marker(
+        html ="""
+    
+            <div>
+            <p><strong>{name}</strong></p>
+            
+            <p style="margin:0;">Airport Code: {ident}</p>
+            <p style="margin:0;">Elevation: {elevation}ft</p>
+            <p style="margin:0;">Location: {city}, {state}</p>
+            
+            <a href="{wiki}" target="_blank">Wikipedia</a>
+            
+            
+
+            </div>
+            """.format( name=data.iloc[i]['name'], 
+                    ident= data.iloc[i]['ident'], 
+                    elevation= data.iloc[i]['elevation_ft'], 
+                    city=data.iloc[i]['municipality'], 
+                    state= data.iloc[i]['iso_region'][-2],
+                    wiki=data.iloc[i]['wikipedia_link'])
+        iframe = folium.IFrame(data.iloc[i]['name'], width=200, height=200)
+
+        tool_tip_html = """<div style="white-space: normal">{name} </div>""".format(name=data.iloc[i]['name'])
+        popup = folium.Popup(iframe, max_width=200)
+        md_icon = folium.features.CustomIcon('img/md_air_icon.png', icon_size=(15,15))
+        md.add_child(folium.Marker(
             location=[float(data.iloc[i]['latitude_deg']), float(data.iloc[i]['longitude_deg'])],
-            popup=popup, icon=md_icon
+            popup=popup, 
+            icon=md_icon,
+            tooltip=folium.Tooltip(
+                text=folium.Html(tool_tip_html, script=True, width=150).render(),
+            )
         ))
 
 map.add_child(lg)
