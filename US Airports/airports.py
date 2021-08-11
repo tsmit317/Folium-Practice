168bf53653fd5d55b433f5a2d69ebc4ba4537e86
Taylor Smith <tjsmith1513@gmail.com>
2021-08-11 12:56:05 -0400

    Added Legend

--- US Airports/airports.py
+++ US Airports/airports.py
@@ -5,6 +5,7 @@ import folium.plugins
 from folium.plugins import fast_marker_cluster
 import pandas as pd
 import os
+import branca
 
 api_key = os.environ.get('MAPBOX_API_KEY')
 
@@ -21,6 +22,58 @@ folium.TileLayer('CartoDB dark_matter', attr='Carto').add_to(map)
 
 lg = folium.FeatureGroup(name="Large Airports", show=False)
 md = folium.FeatureGroup(name="Medium Airports", show=False)
+
+
+import branca
+
+legend_html = """
+{% macro html(this, kwargs) %}
+<div style="
+    position: fixed;
+    bottom: 300px;
+    left: 50px;
+    width: 280px;
+    height: 80px;
+    z-index:9999;
+    font-size:14px;
+    ">
+    <p><img src="img/aa-primary.png" alt="Spirit Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;American Airlines Major Hub</p>
+    <p><img src="img/delta-icon2.png" alt="Spirit Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Delta Airlines Major Hub</p>
+    <p><img src="img/delta-tail.png" alt="Spirit Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Delta Airlines Minor Hub</p>
+    <p><img src="img/spirit.png" alt="Spirit Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Spirit Focus City</p>
+    <p><img src="img/southwest-prim-blue.png" alt="Southwest Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Southwest Operating Bases</p>
+    <p><img src="img/PinClipart.com_southwest-clip-art_3401242.png" alt="Southwest Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Southwest Focus City</p>
+    <p><img src="img/united-prim-white.png" alt="United Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;United Airlines Major Hub</p>
+    <p><img src="img/alaska-face.png" alt="Alaska Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Alaska Airlines Major Hub</p>
+    <p><img src="img/alaska-tail.png" alt="Alaska Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Alaska Airlines Minor Hub</p>
+
+    <p><img src="img/hawaiian2.png" alt="Hawaiian Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Hawaiian Airlines Hub</p>
+    <p><img src="img/Frontier-Airlines-Emblem.png" alt="Frontier Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Frontier Airlines Operating Base</p>
+    <p><img src="img/frontier-airlines.png" alt="Frontier Logo" width="20" height="20" style="margin-left: 20px"></a>&emsp;Frontier Airlines Focus City</p>
+
+    
+    
+</div>
+<div style="
+    position: fixed;
+    bottom: 10px;
+    left: 50px;
+    width: 280px;
+    height: 380px;
+    z-index:9998;
+    font-size:14px;
+    background-color: #ffffff;
+    filter: blur(8px);
+    -webkit-filter: blur(8px);
+    opacity: 0.9;
+    ">
+</div>
+{% endmacro %}
+"""
+
+legend = branca.element.MacroElement()
+legend._template = branca.element.Template(legend_html)
+map.add_child(legend)
 
 def create_popup_html(dataframe):
     return """
@@ -48,42 +101,35 @@ for i in range(len(md_lg_df)):
     icon = folium.features.CustomIcon('img/lg_air_icon.png' if md_lg_df.iloc[i]['type'] == 'large_airport' else 'img/md_air_icon.png', 
                                      icon_size=(25,25) if md_lg_df.iloc[i]['type'] == 'large_airport' else (15,15))
     
-    if md_lg_df.iloc[i]['type'] == 'large_airport':
-        lg.add_child(folium.Marker(
-            location=lat_lon,
-            popup=popup, 
-            icon=icon,
-            tooltip=folium.Tooltip(
-                text=folium.Html(tool_tip_html, script=True, width=150).render(),
-            )
-        ))
-    else:
-        md.add_child(folium.Marker(
-            location=lat_lon,
-            popup=popup, 
-            icon=icon,
-            tooltip=folium.Tooltip(
-                text=folium.Html(tool_tip_html, script=True, width=150).render(),
-            )
-        ))
+    marker = folium.Marker(location=lat_lon,
+                           popup=popup, 
+                           icon=icon,
+                           tooltip=folium.Tooltip(
+                                text=folium.Html(tool_tip_html, script=True, width=150).render()))
 
-def create_hub_layer(hub_list, main_hub, fgroup, sub_hub_list= [], primary_icon='img/lg_air_icon.png', secondary_icon='img/secondary.png'):
+    if md_lg_df.iloc[i]['type'] == 'large_airport':
+        lg.add_child(marker)
+    else:
+        md.add_child(marker)
+
+
+def create_hub_layer(hub_list, fgroup, sub_hub_list= [], primary_icon='img/lg_air_icon.png', secondary_icon='img/secondary.png'):
    
     airline_df = md_lg_df[md_lg_df['ident'].isin(hub_list)]
-    main_hub_coord = [float(md_lg_df.loc[md_lg_df['ident'] == main_hub]['latitude_deg']), float(md_lg_df.loc[md_lg_df['ident'] == main_hub]['longitude_deg'])]
+    main_hub_coord = [float(airline_df.loc[airline_df['ident'] == hub_list[0]]['latitude_deg']), float(airline_df.loc[airline_df['ident'] == hub_list[0]]['longitude_deg'])]
     hub_coord_list = []
 
     for i in range(len(airline_df)):
-        if airline_df.iloc[i]['ident'] != main_hub:
+        if airline_df.iloc[i]['ident'] != hub_list[0]:
             temp = [
                     main_hub_coord,
                     [float(airline_df.iloc[i]['latitude_deg']), float(airline_df.iloc[i]['longitude_deg'])]
                    ]
             hub_coord_list.append(temp)
 
-        popup_html, tool_tip_html = create_popup_html(airline_df.iloc[i])
         lat_lon = [float(airline_df.iloc[i]['latitude_deg']), float(airline_df.iloc[i]['longitude_deg'])]
-    
+       
+        popup_html, tool_tip_html = create_popup_html(airline_df.iloc[i])
         iframe = folium.IFrame(html=popup_html, width=200, height=200)
         popup = folium.Popup(iframe, max_width=200)
 
@@ -100,44 +146,39 @@ def create_hub_layer(hub_list, main_hub, fgroup, sub_hub_list= [], primary_icon=
     return hub_coord_list
 
 
-american_hubs_fg = folium.FeatureGroup( name="American Airlines Hubs", show=False)
-delta_hubs_fg = folium.FeatureGroup( name="Delta Airlines Hubs", show=False)
-united_hubs_fg = folium.FeatureGroup( name="United Airlines Hubs", show=False)
-alaskan_hubs_fg = folium.FeatureGroup( name="Alaska Airlines Hubs", show=False)
-hawaiian_hubs_fg = folium.FeatureGroup( name="Hawaiian Airlines Hubs", show=False)
-frontier_hubs_fg = folium.FeatureGroup( name="Frontier Airlines Hubs", show=False)
 
 # Create American Airline Hub Layer
 american_hubs_fg = folium.FeatureGroup( name="American Airlines Hubs", show=False)
+#main hub is first in list
 aa_list = ['KDFW','KCLT', 'KPHL', 'KORD', 'KLAX', 'KLGA', 'KMIA', 'KPHX', 'KDCA', 'KJFK']
-american_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(aa_list, 'KDFW', american_hubs_fg, [], 'img/aa-primary.png', 'img/aa-secondary.png'), color= '#B61F23'))
+american_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(aa_list, american_hubs_fg, [], 'img/aa-primary.png', 'img/aa-secondary.png'), color= '#B61F23'))
 map.add_child(american_hubs_fg)
 
 # Create Delta Hub Layer    
 delta_hubs_fg = folium.FeatureGroup( name="Delta Airlines Hubs", show=False)
 delta_list = [ 'KATL', 'KBOS', 'KDTW', 'KLAX', 'KMSP', 'KJFK', 'KLGA', 'KSLC', 'KSEA', 'KAUS', 'KCVG', 'KBNA', 'KRDU', 'KSJC']
 delta_list_small = ['KAUS', 'KCVG', 'KBNA', 'KRDU', 'KSJC']
-delta_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(delta_list, 'KATL', delta_hubs_fg, delta_list_small, 'img/delta-icon2.png', 'img/delta-tail.png'), color='#003268'))
+delta_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(delta_list, delta_hubs_fg, delta_list_small, 'img/delta-icon2.png', 'img/delta-tail.png'), color='#003268'))
 map.add_child(delta_hubs_fg)
 
 # United Airlines Hub Layer
 united_hubs_fg = folium.FeatureGroup( name="United Airlines Hubs", show=False)
 united_list = ['KORD', 'KDEN', 'KIAH', 'KLAX', 'KEWR', 'KSFO', 'KIAD']
-united_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(united_list, 'KORD', united_hubs_fg, [], 'img/united-prim-white.png', 'img/united-second-yellow.png'), color='#00fff7'))
+united_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(united_list, united_hubs_fg, [], 'img/united-prim-white.png', 'img/united-second-yellow.png'), color='#00fff7'))
 map.add_child(united_hubs_fg)
 
 # Alaskan Airline Hub Layer
 alaskan_hubs_fg = folium.FeatureGroup( name="Alaska Airlines Hubs", show=False)
 alaskan_list = ['KSEA', 'PANC', 'KLAX', 'KPDX', 'KSFO', 'KSAN', 'KSJC']
 alaskan_list_small = ['KSAN', 'KSJC']
-alaskan_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(alaskan_list, 'KSEA', alaskan_hubs_fg, alaskan_list_small, 'img/alaska-face.png', 'img/alaska-tail.png'), color='#B3D57D', pulseColor='#00385F'))
+alaskan_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(alaskan_list, alaskan_hubs_fg, alaskan_list_small, 'img/alaska-face.png', 'img/alaska-tail.png'), color='#B3D57D', pulseColor='#00385F'))
 map.add_child(alaskan_hubs_fg)
 
 #Hawaiian Airlines Hubs
 hawaiian_hubs_fg = folium.FeatureGroup( name="Hawaiian Airlines Hubs", show=False)
 hawaiian_list = ['PHNL', 'PHOG', 'PHKO', 'PHLI']
 hawaiian_small_list = ['PHKO', 'PHLI']
-hawaiian_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(hawaiian_list, 'PHNL', 
+hawaiian_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(hawaiian_list, 
                                             hawaiian_hubs_fg, hawaiian_small_list,'img/hawaiian2.png', 'img/hawaiian2.png'), color= '#413691'))
 map.add_child(hawaiian_hubs_fg)
 
@@ -145,28 +186,28 @@ map.add_child(hawaiian_hubs_fg)
 frontier_hubs_fg = folium.FeatureGroup( name="Frontier Airlines Hubs", show=False)
 frontier_list = ['KDEN', 'KATL', 'KORD', 'KCLE', 'KCVG', 'KLAS', 'KMCO', 'KPHL', 'KRDU', 'KTTN']
 frontier_small_list = frontier_list[1:]
-frontier_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(frontier_list, 'KDEN', frontier_hubs_fg, frontier_small_list, 'img/Frontier-Airlines-Emblem.png', 'img/Frontier-Airlines.png'), color='#248168'))
+frontier_hubs_fg.add_child(folium.plugins.AntPath(create_hub_layer(frontier_list, frontier_hubs_fg, frontier_small_list, 'img/Frontier-Airlines-Emblem.png', 'img/Frontier-Airlines.png'), color='#248168'))
 map.add_child(frontier_hubs_fg)
 
 #JetBlue Operating
 jetblue_hubs_fg = folium.FeatureGroup(name='JetBlue Focus Airports', show=False)
 jetblue_list = ['KJFK', 'KBOS', 'KFLL', 'KLGB', 'KMCO', 'KSJU']
 jetblue_small_list = jetblue_list[1:]
-temp = create_hub_layer(jetblue_list, 'KJFK', jetblue_hubs_fg, jetblue_small_list, 'img/jb-prim.png', 'img/jetblue-tail2.png')
+temp = create_hub_layer(jetblue_list, jetblue_hubs_fg, jetblue_small_list, 'img/jb-prim.png', 'img/jetblue-tail2.png')
 map.add_child(jetblue_hubs_fg)
 
 #Southwest Airlines Focus Airports
 southwest_hubs_fg = folium.FeatureGroup(name='Southwest Airlines Operating Bases and Focus Cities', show=False)
 southwest_list = ['KATL', 'KBWI', 'KMDW', 'KDAL', 'KDEN', 'KHOU', 'KLAS', 'KLAX', 'KOAK', 'KMCO', 'KPHX','KAUS', 'KFLL', 'KBNA', 'KSMF', 'KSAN', 'KSJC', 'KSTL', 'KTPA']
 southwest_small_list = ['KAUS', 'KFLL', 'KBNA', 'KSMF', 'KSAN', 'KSJC', 'KSTL', 'KTPA']
-temp = create_hub_layer(southwest_list, 'KATL', southwest_hubs_fg, southwest_small_list, 'img/sw-prim.png','img/PinClipart.com_southwest-clip-art_3401242.png' )
+temp = create_hub_layer(southwest_list, southwest_hubs_fg, southwest_small_list, 'img/sw-prim.png','img/PinClipart.com_southwest-clip-art_3401242.png' )
 map.add_child(southwest_hubs_fg)
 
 #Spirit Airlines
 spirit_hubs_fg = folium.FeatureGroup(name='Spirit Arilines Focus Airports', show=False)
 spirit_list = ['KACY', 'KORD', 'KDFW', 'KDTW', 'KFLL', 'KLAS', 'KMCO']
 spirit_small_list = spirit_list
-temp = create_hub_layer(spirit_list, 'KACY', spirit_hubs_fg, spirit_small_list, 'img/spirit.png', 'img/spirit.png')
+temp = create_hub_layer(spirit_list, spirit_hubs_fg, spirit_small_list, 'img/spirit.png', 'img/spirit.png')
 map.add_child(spirit_hubs_fg)
 
 
