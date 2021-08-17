d1eded1a651fae4d59146fd7c24d0c87ca97ff1c
Taylor Smith <tjsmith1513@gmail.com>
2021-08-17 10:22:54 -0400

    Added Stations

--- Amtrak/amtrak.py
+++ Amtrak/amtrak.py
@@ -1,13 +1,31 @@
+from os import name
 import folium
 import folium.plugins
 
 map = folium.Map(location=[40.86736256063699, -94.73893921691446], zoom_start=6)
 
-fvg = folium.FeatureGroup(name='Amtrak Routes')
-fvg.add_child(folium.GeoJson(data=open('Amtrak_Routes.geojson', 'r', encoding='utf-8-sig').read()))
+amtrak_route = folium.FeatureGroup(name='Amtrak Routes')
+amtrak_route.add_child(folium.GeoJson(data=open('Amtrak_Routes.geojson', 'r', encoding='utf-8-sig').read()))
+
+
+red_style = {'fillColor': '#db0000', 'color': '#000000'} 
+station_geo = folium.GeoJson(data=open('Amtrak_Stations.geojson', 'r', encoding='utf-8-sig').read(),
+                            marker = folium.CircleMarker(radius = 4, # Radius in metres
+                                                         weight = 1, #outline weight
+                                                         fill_color = '#000000', 
+                                                         fill_opacity = 1),
+                            style_function=lambda x: red_style,
+                            highlight_function=lambda feature: {"fillcolor": "ff0000", "color": "white"},
+                            popup=folium.GeoJsonPopup(fields= ['STNNAME'],
+                            aliases= ['Station: ']))
+
+
+amtrak_stations = folium.FeatureGroup(name='Amtrak Stations', show=True)
+amtrak_stations.add_child(station_geo)
 
 mini_map = folium.plugins.MiniMap(toggle_display = True)
-map.add_child(fvg)
+map.add_child(amtrak_route)
+map.add_child(amtrak_stations)
 map.add_child(mini_map)
 map.add_child(folium.LayerControl())
 
