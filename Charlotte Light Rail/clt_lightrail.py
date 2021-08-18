fe6bcd2abfcc9cfcb32c1c3ed0043ffc9a68dc4d
Taylor Smith <tjsmith1513@gmail.com>
2021-08-18 14:53:07 -0400

    Added Blue Line Stations. Added source to title.

--- Charlotte Light Rail/clt_lightrail.py
+++ Charlotte Light Rail/clt_lightrail.py
@@ -1,18 +1,58 @@
+from os import name
 import folium
+from folium.map import Popup
 import folium.plugins
+import pandas as pd
 
 
-loc = 'Charlotte Public Transportation'
+
+
 title_html = '''
-             <h3 align="center" style="font-size:16px"><b>{}</b></h3>
-             '''.format(loc)   
+             <h3 align="center" style="font-size:16px"><b>Charlotte Public Transportation</b></h3>
+             <h6 align="center" style="font-size:14px"><b>Data Source: 
+                <a href="https://data.charlottenc.gov/search?categories=charlotte%20area%20transit%20system&groupIds=7f5968deb9bc435d851074c93cea6092" target="_blank">
+                Charlotte Open Data Portal </a></b></h6>
+             '''
 
 
 map = folium.Map(location=[35.245460799023334, -80.8343778182691], zoom_start=12)
 
+blue_style = {'fillColor': '#0320fc', 'color': '#0320fc', 'weight': 5} 
+
+def create_popup_html(dataframe):
+    return """
+    <div style="width: 200px;">
+        <p style="margin:0;"><b>Station:</b> {name}</p>
+        <p style="margin:0;"><b>Platform:</b> {platform}</p>
+        <p style="margin:0;"><b>Park and Ride:</b> {par}</p>
+        <p style="margin:0;"><b>Parking Spaces:</b> {ps}</p>
+        <p style="margin:0;"><b>Bike Locker:</b> {bl}</p>
+        <p style="margin:0;"><b>Bike Rack:</b> {br}</p>
+          
+    </div>
+    """.format( name=str(dataframe['NAME']), 
+                platform= str(dataframe['Platform']), 
+                par=str(dataframe['Park and Ride']), 
+                ps= str(dataframe['Parking Spaces']),
+                bl=str(dataframe['Bike Locker']),
+                br=str(dataframe['Bike Rack'])), """<div style="white-space: normal">{name} </div>""".format(name=dataframe['NAME'])
+
+
+df = pd.read_csv('data/LYNX_Blue_Line_Stations.csv')
+blue_stations_fg = folium.FeatureGroup(name='Blue Line Stations')
+for i in range(len(df)):
+    popup_html, tool_tip_html = create_popup_html(df.iloc[i])
+    folium.CircleMarker(location=[ float(df.iloc[i]['lat']), float(df.iloc[i]['long'])],
+                        popup=popup_html,
+                        tooltip=tool_tip_html,
+                        radius = 6, # Radius in metres
+                        weight = 1, #outline weight
+                        fill_color = '##0320fc', 
+                        fill_opacity = 1,
+                        style_function=lambda x: blue_style,
+                        highlight_function=lambda x: {"fillcolor": "ff0000", "color": "white"}).add_to(blue_stations_fg)
 
 blue = folium.FeatureGroup(name='Blue Line')
-blue_style = {'fillColor': '#0320fc', 'color': '#0320fc', 'weight': 5} 
 blue.add_child(folium.GeoJson(data=open('data/LYNX_Blue_Line_Route.geojson', 'r', encoding='utf-8-sig').read(), 
                 style_function=lambda x: blue_style))
 
@@ -62,6 +102,7 @@ mini_map = folium.plugins.MiniMap(toggle_display = True)
 
 map.add_child(silver)
 map.add_child(blue)
+map.add_child(blue_stations_fg)
 map.add_child(red_line_route)
 map.add_child(red_line_stops)
 map.add_child(gold)
