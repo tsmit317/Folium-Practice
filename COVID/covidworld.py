360eced8bef87b8e0ad840252677de72b4b04e34
Taylor Smith <tjsmith1513@gmail.com>
2021-08-19 12:05:43 -0400

    Added title.

--- COVID/covidworld.py
+++ COVID/covidworld.py
@@ -4,6 +4,7 @@ import pandas as pd
 df = pd.read_csv('owid-covid-data.csv')
 covid_df = df.loc[(df['date'] == '2021-08-16') & (df['total_deaths'].notnull()) & (df['location'] != 'World'), ['location', 'total_deaths']] 
 covid_df.loc[covid_df['location'] == 'United States', 'location'] = 'United States of America'
+covid_df.loc[covid_df['location'] == 'Czechia', 'location'] = 'Czech Republic'
 covid_df.astype({'location': 'str', 'total_deaths':'float'})
 
 
@@ -13,21 +14,32 @@ country_shapes = f'{url}/world-countries.json'
 
 map = folium.Map(location=[48, -102], zoom_start=3)
 
-# folium.GeoJson(data=open('countries.geojson', 'r', encoding='utf-8-sig').read()),
-myscale = (covid_df['total_deaths'].quantile((0,.1,.2,.4,.5,.6,.7,.8,.9,1))).tolist()
-folium.Choropleth(
+# myscale = (covid_df['total_deaths'].quantile((0,.1,.2,.4,.5,.6,.7,.8,.9,1))).tolist()
+choro = folium.Choropleth(
     geo_data=country_shapes,
     name="choropleth",
     data=covid_df,
     columns=['location', 'total_deaths'],
     key_on="feature.properties.name",
     fill_color="YlOrRd",
-    threshold_scale=myscale,
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name="Total Fully Vaccinated",
-).add_to(map)
+    legend_name="Total Deaths",
+    highlight=True
+)
 
+choro.geojson.add_child(folium.features.GeoJsonTooltip(
+        fields=['name'],
+        aliases=['Country'],
+        style=('background-color: grey; color: white;'),
+        localize=True
+        ))
+map.add_child(choro)
 folium.LayerControl().add_to(map)
-
+title_html = '''
+             <h3 align="center" style="font-size:16px"><b>COVID-19 Choropleth Map</b></h3>
+             <h6 align="center" style="font-size:14px"><b>Data Source: 
+                <a href="https://ourworldindata.org/coronavirus" target="_blank"> Our World in Data</a></b></h6>
+             '''
+map.get_root().html.add_child(folium.Element(title_html))
 map.save('covid-world.html')
